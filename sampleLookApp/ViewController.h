ec04afd4be7c870167f9a73b032d2f946c777259
daikiobara <dobara.gc4@gmail.com>
2015-01-19 14:28:56 +0800

    Initial Commit

---
+++ sampleLookApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleLookApp
+//
+//  Created by Daiki  on 2015/01/19.
+//  Copyright (c) 2015年 Daiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sampleLookApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sampleLookApp
+//
+//  Created by Daiki  on 2015/01/19.
+//  Copyright (c) 2015年 Daiz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
